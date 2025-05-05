ff9eab714f6856cc81623912b7a66e56aeabb8da
Anvar331 <anvar.intezar@gmail.com>
2025-05-05 21:06:36 +0300

    GetCategories component update+App component update

--- app-name/src/components/GetCatergories.tsx
+++ app-name/src/components/GetCatergories.tsx
@@ -4,9 +4,35 @@ function GetCategories() {
 
     const [category, setCategory] = useState([]);
 
-    fetch('https://dummyjson.com/products/category-list')
- .then(res => res.json())
- .then(data => console.log(data));
+    useEffect(() => {
+        categories()
+    },[])
+
+    const categories = async () => {
+        const response = await fetch ('https://dummyjson.com/products/category-list');
+        setCategory(await response.json())
+    }
+
+    return (
+        <div>
+          <h1> Here I am showing all categories</h1>
+          <ol className="list of categories">
+
+            {category.map((data) => {
+             return(
+                <li className="list group-item" key={data.id}> {data.title} </li>
+             )
+        })}
+
+        </ol>
+        </div>
+    )
+
  }
 
  export default GetCategories;
+
+
+ //fetch('https://dummyjson.com/products/category-list')
+ //.then(res => res.json())
+ //.then(data => console.log(data));
