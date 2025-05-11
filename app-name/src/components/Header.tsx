a11cacfe1423da6b1ee46568a42abca7bfc4f84a
Anvar331 <anvar.intezar@gmail.com>
2025-05-11 19:39:30 +0300

    old project files + new components

--- app-name/src/components/Header.tsx
+++ app-name/src/components/Header.tsx
@@ -1,9 +1,17 @@
-const Header = () => {
+function header () {
     return (
-      <div>
-        <h2>The Header</h2>
-      </div>
-    );
-  };
-  
-  export default Header;
+        <header>
+          <nav role="full-horizontal">
+           <ul className="container">
+            <li><a href="homepage.html">HOME</a></li> &nbsp; &nbsp;
+            <li><a id = "About Us" href="about.html">ABOUT US</a></li> &nbsp; &nbsp;
+            <li><a href="FAQ.html">FAQ</a></li> &nbsp; &nbsp;
+            <li><a href="#privacypolicy">PRIVACY POLICY</a></li> &nbsp; &nbsp;
+            <li><a href="#contactcus">CONTACT US</a></li>
+           </ul>
+           </nav>
+        </header> 
+    )
+}
+
+export default header
